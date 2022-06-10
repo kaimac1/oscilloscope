1c532fbc2022514f7c3c5c7e4f05a2d5a553fd6d
Kyle McInnes <kylesrmcinnes@gmail.com>
2022-06-10 18:40:10 +0100

    async acquisition mode

--- scope/__init__.py
+++ scope/__init__.py
@@ -14,9 +14,11 @@ SCOPE_HEIGHT = 128
 
 SCALES = [[ADC.ATTN_0DB, 100], [ADC.ATTN_6DB, 200], [ADC.ATTN_11DB, 400]]
 TIMEBASES = [[31, 1], [62, 2], [156, 5], [312, 10], [625, 20], [1562, 50], [3125, 100], [6250, 200], [0, 1000], [0, 2000], [0, 5000], [0, 10000], [0, 20000], [0, 60000]]
+ACQUIRE_MODE_ASYNC_THRESH = 101
 PX_PER_VDIV = 16
 PX_PER_HDIV = 32
 
+TRIGGER_WIDTH = 1
 MAX_TRIGGER_CYCLES = 256
 
 
@@ -30,7 +32,7 @@ class MyApp(TextApp):
         self.set_rotation(270)
 
     def on_activate(self):
-        super().on_activate() # This will clear the screen by calling TextWindow.redraw()
+        super().on_activate()
         display.fill_rect(SAMPLES, 0, 240-SAMPLES, SCOPE_HEIGHT+1, GREY)
 
         self.scale = 0
@@ -41,11 +43,8 @@ class MyApp(TextApp):
         self.roll_mode = False
         self.roll_display_cnt = 0
         self.adc_init()
+        self.source = self.get_adc
         self.draw_info()
-
-        self.outpin = G3
-        self.outpin.init(self.outpin.OUT, self.outpin.PULL_DOWN)
-        self.outpin.value(0)
 
         self.buttons.set_rotation(0) # Something seems to be broken with the buttons when the screen is rotated 90/270 degrees...
         # self.buttons.on_press(BUTTON_A, lambda: self.buttona())
@@ -58,7 +57,6 @@ class MyApp(TextApp):
 
     def clear_buffer(self):
         self.buffer0 = bytearray(SAMPLES)
-
 
     def btn_ud(self, dx):
         if BUTTON_A.value() == 1:
@@ -74,7 +72,6 @@ class MyApp(TextApp):
             if self.roll_mode:
                 self.clear_buffer()
             self.draw_info()
-
 
     def timebase_set(self, dt):
         if 0 <= (self.timebase + dt) < len(TIMEBASES):
@@ -102,6 +99,7 @@ class MyApp(TextApp):
                 self.clear_buffer()
             self.roll_mode = True
             self.roll_timer = self.periodic(ms_per_div/PX_PER_HDIV, self.acquire_rollmode)
+            self.trig = False
         else:
             if not self.roll_mode: return
             self.roll_mode = False
@@ -112,6 +110,9 @@ class MyApp(TextApp):
 
     # Acquisition
 
+    def get_adc(self):
+        return self.adc0.read_uv() // self.vscaling
+
     def acquire_buffer(self, buffer, n, timebase_setting):
         ts_microseconds = TIMEBASES[timebase_setting][0]
 
@@ -119,13 +120,13 @@ class MyApp(TextApp):
         if timebase_setting == 0:
             x = 0
             for i in range(n):
-                buffer[i] = self.adc0.read_uv() // self.vscaling
+                buffer[i] = self.source()
                 x = i * 0.33 # nop()
             return
 
         for i in range(n):
             t0 = time.ticks_us()
-            buffer[i] = self.adc0.read_uv() // self.vscaling
+            buffer[i] = self.source()
             # Improve timebase accuracy by factoring in the acquisition time
             # 50 Hz noise on 20ms/div setting should line up with the grid lines
             t_adc = time.ticks_diff(time.ticks_us(), t0)
@@ -134,11 +135,12 @@ class MyApp(TextApp):
     def acquire_rollmode(self):
         for i in range(SAMPLES-1):
             self.buffer0[i] = self.buffer0[i+1]
-        self.buffer0[SAMPLES-1] = self.adc0.read_uv() // self.vscaling
+        self.buffer0[SAMPLES-1] = self.source()
 
         self.roll_display_cnt -= 1
         if self.roll_display_cnt < 0:
             self.after(0, self.draw_samples)
+            # For faster rolling timebases, don't draw every new sample
             if TIMEBASES[self.timebase][1] == 2000:
                 self.roll_display_cnt = 1
             elif TIMEBASES[self.timebase][1] == 1000:
@@ -146,14 +148,22 @@ class MyApp(TextApp):
             else:
                 self.roll_display_cnt = 0
 
+    def acquire_async(self):
+        self.buffer0[self.bufidx] = self.adc0.read_uv() // self.vscaling
+        self.bufidx += 1
+        if self.bufidx == SAMPLES:
+            self.acq_timer.cancel()
+            self.after(0, self.draw_samples)
+
     def acquisition_start(self):
+        # Try to trigger, give up after MAX_TRIGGER_CYCLES
         trig_attempts = 0
         trig_state = 0
         triggered = 0
-        trigbuf = bytearray(1)
+        trigbuf = bytearray(TRIGGER_WIDTH)
         trig_level = int(self.trig_voltage * 1000000) // self.vscaling
         while trig_attempts < MAX_TRIGGER_CYCLES:
-            self.acquire_buffer(trigbuf, 1, 0)
+            self.acquire_buffer(trigbuf, TRIGGER_WIDTH, 0)
             if trig_state == 0:
                 triggered = 1
                 for val in trigbuf:
@@ -168,8 +178,13 @@ class MyApp(TextApp):
 
             trig_attempts += 1
 
-        self.acquire_buffer(self.buffer0, SAMPLES, self.timebase)
-        self.after(0, self.draw_samples)
+        # If the timebase is too long, get the samples in a timer callback so the UI stays responsive to button presses
+        if TIMEBASES[self.timebase][1] >= ACQUIRE_MODE_ASYNC_THRESH:
+            self.bufidx = 0
+            self.acq_timer = self.periodic(TIMEBASES[self.timebase][0] / 1000, self.acquire_async)
+        else:
+            self.acquire_buffer(self.buffer0, SAMPLES, self.timebase)
+            self.after(0, self.draw_samples)
 
 
     # Drawing
